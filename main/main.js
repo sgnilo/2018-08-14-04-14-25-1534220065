7a0368ded6dc5fcb45e39ac11b6f36fec7c90ac0
sgnilo <43700959+sgnilo@users.noreply.github.com>
2019-05-31 16:36:30 +0800

    Update and rename main/main.js to  main/main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,16 @@
-module.exports = function main() {
+module.exports = function main(a) {
     console.log("Debug Info");
-    return 'Hello World!';
+    var top='',middle='',bottom='',b=[{t:'._.',m:'|.|',b:'|_|'},{t:'...',m:'..|',b:'..|'},{t:'._.',m:'._|',b:'|_.'},{t:'._.',m:'._|',b:'._|'},{t:'...',m:'|_|',b:'..|'},{t:'._.',m:'|_.',b:'._|'},{t:'._.',m:'|_.',b:'|_|'},{t:'._.',m:'..|',b:'..|'},{t:'._.',m:'|_|',b:'|_|'},{t:'._.',m:'|_|',b:'..|'}],c=[]
+    for (let i=0;i<a.length;i++){
+    	c.push(parseInt(a.charAt(i)))    }
+    for (let i=0;i<a.length;i++){
+    	top=top+b[c[i]].t+' '
+    	middle=middle+b[c[i]].m+' '
+    	bottom=bottom+b[c[i]].b+' '
+    }
+    top=top.trim()+'\n'
+    middle=middle.trim()+'\n'
+    bottom=bottom.trim()+'\n'
+    // console.log(d)
+    return top+middle+bottom
 };
